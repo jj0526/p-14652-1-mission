320a20895c3c19552288a73f36decbe5b9ba9228
jj <syeonie0526@gmail.com>
2025-12-31 14:09:05 +0900

    0004 - 이벤트 리스너에서 수신, 토픽과 그룹 아이디의 조합이 같은 핸들러는 하나의 이벤트 발생에 대해서 중복실행되지 않는다

---
+++ src/main/java/com/back/boundedContext/post/in/PostEventListener.java
@@ -0,0 +1,26 @@
+package com.back.boundedContext.post.in;
+
+
+import com.back.shared.member.event.MemberJoinedEvent;
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class PostEventListener {
+    @KafkaListener(topics = "MemberJoinedEvent", groupId = "PostEventListener__handle__1")
+    public void handle(MemberJoinedEvent event) {
+        System.out.println("handle, MemberJoinedEvent, member: " + event.memberDto().id());
+    }
+
+    @KafkaListener(topics = "MemberJoinedEvent", groupId = "PostEventListener__handle__2")
+    public void handle2(MemberJoinedEvent event) {
+        System.out.println("handle2, MemberJoinedEvent, member: " + event.memberDto().id());
+    }
+
+    @KafkaListener(topics = "MemberJoinedEvent", groupId = "PostEventListener__handle__2")
+    public void handle2Again(MemberJoinedEvent event) {
+        System.out.println("handle2-again, MemberJoinedEvent, member: " + event.memberDto().id());
+    }
+}
